35a37ee487a5907f34945dfd385c6cfd8589fcc4
Daryna Shvydka <dshvydka@1-C-20.42heilbronn.de>
2024-11-07 14:32:38 +0100

    signed integers conversion

--- src/ft_printf_num_s.c
+++ src/ft_printf_num_s.c
@@ -1,25 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf_num_s.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dshvydka <dshvydka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/31 21:23:39 by dshvydka          #+#    #+#             */
-/*   Updated: 2024/11/07 14:29:52 by dshvydka         ###   ########.fr       */
+/*   Created: 2024/11/05 16:24:57 by dshvydka          #+#    #+#             */
+/*   Updated: 2024/11/07 14:31:15 by dshvydka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#include "libftprintf.h"
 
-# include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
+// % d Prints a decimal(base 10) number.
+// % i Prints an integer in base 10.
 
-int	ft_printf(const char *format, ...);
-int	ft_printf_char(int c);
-int	ft_printf_str(char *str);
-int	ft_printf_num_s(int num);
+int	ft_printf_num_s(int num)
+{
+	char	*str;
+	int		len;
 
-#endif
+	len = 0;
+	str = ft_itoa(num);
+	if (str)
+	{
+		len = ft_strlen(str);
+		write(1, str, len);
+		free(str);
+	}
+	else
+		return (0);
+	return (len);
+}
